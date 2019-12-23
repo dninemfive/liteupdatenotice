4837b4967d67fd2ddedab17d0bfc768bc009630d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-12-23 11:46:02 -0800

    restructure

--- Source/LiteUpdateNotice/GameComponent_Notifier.cs
+++ Source/LiteUpdateNotice/GameComponent_Notifier.cs
@@ -16,6 +16,11 @@ namespace LiteUpdateNotice
         public GameComponent_Notifier(Game game)
         {
         }
+
+        public override void ExposeData()
+        {
+            //do nothing to avoid breaking saves
+        }
         public override void StartedNewGame()
         {
             base.StartedNewGame();
@@ -28,31 +33,11 @@ namespace LiteUpdateNotice
         }
         public void TrySendNotification()
         {
-            if(!LoadTracker.NotificationSent)
+            if (!LoadTracker.NotificationSent)
             {
-                foreach (NoticeDef def in DefDatabase<NoticeDef>.AllDefsListForReading)
-                {
-                    Log.Message(": " + def.defName + " | " + def.version + " | " + assemblyVersion[def]);
-                    if (!assemblyVersion.ContainsKey(def))
-                    {
-                        Log.Message("Added");
-                        assemblyVersion.Add(def, def.version);
-                    }
-                    if (!LetterStackContains(def.letter) && assemblyVersion[def] != def.version)
-                    {
-                        Find.LetterStack.ReceiveLetter(def.LabelCap, def.description, def.letter);
-                        assemblyVersion[def] = def.version;
-                    }
-                }
+                foreach (NoticeDef def in DefDatabase<NoticeDef>.AllDefsListForReading) if (!LetterStackContains(def.letter)) Find.LetterStack.ReceiveLetter(def.LabelCap, def.description, def.letter);
                 LoadTracker.NotificationSent = true;
             }
         }
-
-        public override void ExposeData()
-        {
-            Scribe_Collections.Look(ref assemblyVersion, "assemblyVersion", LookMode.Def, LookMode.Value);
-        }
-
-        private Dictionary<NoticeDef, string> assemblyVersion = new Dictionary<NoticeDef, string>();
     }
 }
---
+++ Source/NoticeDef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace LiteUpdateNotice
+{
+    class NoticeDef : Def
+    {
+        #pragma warning disable CS0649
+        public LetterDef letter;
+        public string version;
+    }
+}
